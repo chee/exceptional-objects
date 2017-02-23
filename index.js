4967963fa1d10a870b5195d6aaeb2c206d23f641
chee <chee@snaek.org>
2017-02-23 21:11:52 +0000

    convert to old fashioned javascript from the 1940s

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const error = key => new Error(`Object does not contain ${key}`)
-module.exports = object => new Proxy(object, {
-  get: (object, key) => {
-    if (key in object) {
-      return object[key]
-    } else {
-      throw error(key)
+module.exports = function (object) {
+  return new Proxy(object, {
+    get: function (object, key) {
+      if (key in object) {
+        return object[key]
+      } else {
+        throw new Error('Object does not contain ' + key)
+      }
     }
-  }
-})
+  })
+}
